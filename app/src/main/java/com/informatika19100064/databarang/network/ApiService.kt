1ed3837fe480dd983f9ee9fd110c4192fb92506d
dayuza <bocilumam@gmail.com>
2022-01-15 10:41:17 +0700

    Pertemuan 15

---
+++ app/src/main/java/com/informatika19100064/databarang/network/ApiService.kt
@@ -0,0 +1,38 @@
+package com.informatika19100064.databarang.network
+
+import com.informatika19100064.databarang.model.ResponseActionBarang
+import com.informatika19100064.databarang.model.ResponseBarang
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+
+interface ApiService {
+    @GET("read.php")
+    fun getBarang(): Call<ResponseBarang>
+
+    @FormUrlEncoded
+    @POST("create.php")
+    fun insertBarang(
+        @Field("nama_barang") namaBarang: String?,
+        @Field("jumlah_barang") jmlBarang: String?
+    ): Call<ResponseActionBarang>
+
+    @FormUrlEncoded
+    @POST("update.php")
+    fun updateBarang(
+        @Field("id") id: String?,
+        @Field("nama_barang") namaBarang: String?,
+        @Field("jumlah_barang") jmlBarang: String?
+    ): Call<ResponseActionBarang>
+
+    @FormUrlEncoded
+    @POST("delete.php")
+    fun deleteBarang(
+        @Field("id") id: String?
+    ): Call<ResponseActionBarang>
+
+
+}
